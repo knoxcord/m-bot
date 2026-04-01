25f447a7e40c2f489bbf56e5816904914057c283
Matt Warman <matt@mkwarman.com>
2026-04-01 11:22:58 -0400

    Add globally mutable feature

--- src/features/spank/config.ts
+++ src/features/spank/config.ts
@@ -4,9 +4,11 @@ import { ConfigurationRegistration } from "src/features/configuration/configurat
 export const MutedRoleIdConfigurationKey = 'MUTED_ROLE_ID';
 export const MutedDurationSecondsConfigurationKey = 'MUTED_DURATION_SECONDS';
 export const RoleIdsThatCanMuteConfigurationKey = 'ROLE_IDS_THAT_CAN_MUTE';
+export const GlobalMutableRoleId = 'GLOBAL_MUTABLE_ROLE_ID';
 
 export const spankConfigurationRegistrations = <ConfigurationRegistration[]>[
     ['Muted Role Id', MutedRoleIdConfigurationKey],
     ['Muted Duration Seconds', MutedDurationSecondsConfigurationKey],
     ['Role Ids That Can Mute', RoleIdsThatCanMuteConfigurationKey],
+    ['Globally Mutable Role Id - Anyone with this role can be slapped by anyone', GlobalMutableRoleId],
 ];
